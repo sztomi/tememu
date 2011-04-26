2ebfb680f989a8f5f90cfa003b457e8994d50e62
Tamás Szelei <szelei.t@gmail.com>
2011-04-26 23:25:33 +0200

    added license and began MipsCPU class

---
+++ mipscpu.cpp
@@ -0,0 +1,39 @@
+/*
+ *    The MIT License
+ *    
+ *    Copyright (c) 2011, Tamás Szelei
+ *    
+ *    Permission is hereby granted, free of charge, to any person obtaining a copy
+ *    of this software and associated documentation files (the "Software"), to deal
+ *    in the Software without restriction, including without limitation the rights
+ *    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *    copies of the Software, and to permit persons to whom the Software is
+ *    furnished to do so, subject to the following conditions:
+ *    
+ *    The above copyright notice and this permission notice shall be included in
+ *    all copies or substantial portions of the Software.
+ *    
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *    THE SOFTWARE.
+ */
+ 
+#include "mipscpu.h"
+#include "consts.h"
+
+#include <iostream>
+
+namespace tememu 
+{
+    MipsCPU::MipsCPU()
+        : _HI(0), _LO(0), _FCSR(0), _PC(0)
+    {
+        _GPR.reserve(gpr_count);
+        _FPR.reserve(fpr_count);
+        _FCR.reserve(fcr_count);        
+    }
+} // tememu
